399850e26077b307bb219ee5235a651e03a1afe6
Slozzyondul <solomonondula@gmail.com>
2024-05-22 15:11:59 +0300

    commit114

--- django_app/myportfolio/project/views.py
+++ django_app/myportfolio/project/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Product
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from .forms import CustomUserCreationForm
+#from .models import CartItem
+
 
 # Create your views here.
 def home_screen(request):
@@ -64,5 +70,33 @@ def password_reset_screen(request):
     return render (request, 'password_reset.html', {})
 
 def product_list_screen(request):
-    products = Product.objects.all()
-    return render (request, 'product_list.html', {'products': products})
+    products = [
+        {
+            'name': f'Product {i}',
+            'price': i * 10,
+            'description': f'Description for Product {i}',
+        } for i in range(1, 500)
+    ]
+    return render(request, 'product_list.html', {'products': products})
+
+
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('index')  # Redirect to the home page or any other page
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'register.html', {'form': form})
+
+
+
+def add_to_cart(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        price = request.POST['price']
+        # Add the product to the cart, either by creating a CartItem instance or updating session/cart data
+        # Redirect to the cart page or product list page
+    return redirect('product_list')
